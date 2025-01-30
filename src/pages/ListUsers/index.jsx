61d63cd44b03c128bd2aa1f2123e5d6d733958ee
devpedroferreira <pedroofan@gmail.com>
2025-01-30 14:16:49 -0300

    added usersList from APi server in pag2

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,23 +1,25 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import api from '../../services/api'
 
 import DefaultButton from "../../components/Button"
 import TopBackGround from "../../components/TopBackGround"
 import { Page2 } from "./styles"
-import { useNavigate } from "react-router-dom"
+import { data, useNavigate } from "react-router-dom"
 import { Title } from "./styles"
 
 
 // get users with hook useEffects
 const ListUsers = () => {
     const navigate = useNavigate()
-    
+    const [ usersFromApi, setUsersFromApi ] = useState([])
+
     // using useEffect when load pag2
     // Additional useEffect if needed
     useEffect(() => {
         const fetchUsers = async () => {
             const {data} = await api.get('/users');
-            console.log(data)
+            
+            setUsersFromApi(data)
         };
         fetchUsers();
         
@@ -31,6 +33,16 @@ const ListUsers = () => {
    
             <Title> Lista de Usuarios: </Title>
             
+            {usersFromApi.map((user) => (
+                <div >
+                    <p> Nome: {user.name}</p>
+                    <p> Idade: {user.age}</p>
+                    <p> Email: {user.email}</p>
+                    <p> <br /></p>
+                </div>
+            ))}
+
+
             <DefaultButton type='button' onClick={ () => navigate('/')}  > Voltar </DefaultButton>
         </div>
 
